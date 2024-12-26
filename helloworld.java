20c93a5e2048881154cca29d98e92431021ea1a1
reachsurendar21 <142766771+reachsurendar21@users.noreply.github.com>
2024-12-26 16:21:14 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch31 team with vinay , pratik  and team1");
-      System.out.println("Hello Batch batch31 WITH FOR STUDENTS RUNNING in front of Ujwal (offer) and Aniket and Kumar sindhu in making CI automated vikas teach devops CI proecss  ");
+     System.out.println("Hello Happy New year to my team");
+      System.out.println("We are from Firstdatacheck, wishing you all a happy new year 2025");
     }  
 } 
